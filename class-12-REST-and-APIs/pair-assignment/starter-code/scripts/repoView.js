5e24f7e50c3929443935968b1b575e12872127c4
Matthew Smith <mattsmith203@hotmail.com>
2016-04-03 11:49:59 -0700

    day12_fixed

--- class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repoView.js
+++ class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repoView.js
@@ -1,7 +1,7 @@
 (function(module) {
   var repoView = {};
 
-  // DONE: Private methods declared here live only within the scope of the wrapping IIFE.
+   // DONE: Private methods declared here live only within the scope of the wrapping IIFE.
   var ui = function() {
     var $about = $('#about'); // Best practice: Cache the DOM query if it's used more than once.
 
@@ -9,22 +9,30 @@
     $about.show().siblings().hide();
   };
 
-  // TODO: Remember that new Handlebars template? Let's compile it!
-  // Save the result in this `render` variable.
-  var render;
+   // TODO: Remember that new Handlebars template? Let's compile it!
+   // Save the result in this `render` variable.
 
-  // DONE: If all the data is loaded, we can prep the UI and render the repos.
+  var render = function(repo) {
+    var template = Handlebars.compile($('#repo-template').text());
+    return template(repo);
+  };
+
+
+
+   // DONE: If all the data is loaded, we can prep the UI and render the repos.
   repoView.index = function() {
     ui();
 
-    // The jQuery `append` method lets us append an entire array of HTML elements at once,
-    // So we can use a little FP to transform our data-set into DOM nodes:
+     // The jQuery `append` method lets us append an entire array of HTML elements at once,
+     // So we can use a little FP to transform our data-set into DOM nodes:
     $('#about ul').append(
-      // REVIEW: we added the `name` property here as our initial filter property. Try
-      // changing it to see what happens!
+       // REVIEW: we added the `name` property here as our initial filter property. Try
+       // changing it to see what happens!
       repos.with('name').map(render)
     );
   };
+
+
 
   module.repoView = repoView;
 })(window);
