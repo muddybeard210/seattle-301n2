e12ee539a4efd769e682ab31451c5feaabc6f2f7
Matthew Smith <mattsmith203@hotmail.com>
2016-04-03 11:15:00 -0700

    tokens

---
+++ class-12-REST-and-APIs/pair-assignment/maddieMatt12/scripts/repo.js
@@ -0,0 +1,37 @@
+(function(module) {
+  var repos = {};
+
+  repos.all = [];
+
+  repos.requestRepos = function(callback) {
+    $.ajax({
+      url: 'https://api.github.com/user/repos',
+      type: 'GET',
+      headers: {
+        'Authorization': 'token ' + repoToken
+      },
+      success: function(data, message, xhr) {
+        console.log(data);
+        console.log(message);
+        repos.all = data;
+        callback();
+      },
+      error: function(data, status) {
+        console.log(data);
+        console.log(status);
+      }
+    });
+    // TODO: How would you like to fetch your repos? Don't forget to call the callback.
+
+  };
+
+  // DONE: Model method that filters the full collection for repos with a particular attribute.
+  // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
+  repos.with = function(attr) {
+    return repos.all.filter(function(repo) {
+      return repo[attr];
+    });
+  };
+
+  module.repos = repos;
+})(window);
